f3edb16e0d159af47315f6b9d2ecd6400ab2af2a
nmuchow <44323989+nmuchow@users.noreply.github.com>
2018-11-18 19:33:14 -0700

    Update tempsensor.cpp

--- tempsensor.cpp
+++ tempsensor.cpp
@@ -34,33 +34,33 @@ void tempSensorLoop(){
 //-------------------------------------//
 //------------------------------------//
 //arduino code
-#include <Wire.h>
-#include "Adafruit_MCP9808.h"
+// #include <Wire.h>
+// #include "Adafruit_MCP9808.h"
 
-// Create the MCP9808 temperature sensor object
-Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+// // Create the MCP9808 temperature sensor object
+// Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("MCP9808 demo");
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println("MCP9808 demo");
   
-  // Make sure the sensor is found, you can also pass in a different i2c
-  // address with tempsensor.begin(0x19) for example
-  if (!tempsensor.begin()) {
-    Serial.println("Couldn't find MCP9808!");
-    while (1);
-  }
-}
+//   // Make sure the sensor is found, you can also pass in a different i2c
+//   // address with tempsensor.begin(0x19) for example
+//   if (!tempsensor.begin()) {
+//     Serial.println("Couldn't find MCP9808!");
+//     while (1);
+//   }
+// }
 
-void loop() {
-  // Read and print out the temperature, then convert to *F
-  float c = tempsensor.readTempC();
-  float f = c * 9.0 / 5.0 + 32;
-  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
-  Serial.print(f); Serial.println("*F");
+// void loop() {
+//   // Read and print out the temperature, then convert to *F
+//   float c = tempsensor.readTempC();
+//   float f = c * 9.0 / 5.0 + 32;
+//   Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
+//   Serial.print(f); Serial.println("*F");
   
-  //Serial.println("Shutdown MCP9808.... ");
-  //tempsensor.shutdown(); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere
+//   //Serial.println("Shutdown MCP9808.... ");
+//   //tempsensor.shutdown(); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere
   
-  delay(1000);
-}
+//   delay(1000);
+// }
